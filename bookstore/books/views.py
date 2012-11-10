13a51abb13d8955daeeab284adb6a7d27bfe9c87
Wenbin Wu <admin@wenbinwu.com>
2012-11-10 15:29:18 +0100

    add paginator

--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage
 
 from .models import Book
 
 
+NUM_PER_PAGE = 10
+
+
 def books(request):
+    books = Book.objects.all()
+    paginator = Paginator(books, NUM_PER_PAGE)
+
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+
+    try:
+        books = paginator.page(page)
+    except EmptyPage:
+        books = paginator.page(paginator.num_pages)
+
     return render(request, 'books/books.html',
-                  {'books': Book.objects.all()})
+                  {'books': books})
